abbe1526ec4ebab5f35308d0a9353f9d887609ff
chilledornaments <mitch@chilledornaments.com>
2025-04-19 08:56:34 -0600

    fix: remove git command

--- queries/1.sql
+++ queries/1.sql
@@ -1,4 +1,3 @@
 --  First names, last names, ages and occupations of all clients
-SELECT
-
-git push -u origin main
+-- ClientDOB is YYYY, so we can subtract it from the current year to get the client's age(ish)
+SELECT ClientFirstName, ClientLastName, (YEAR(CURDATE()) - ClientDOB) as age, Occupation from Client;
